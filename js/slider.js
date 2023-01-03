58102b95441bec15f6898698137a0b239d082f77
Daniele Campari <danielecampari@gmail.com>
2023-01-03 16:57:19 +0100

    Add showSlide function (slide.js)

--- js/slider.js
+++ js/slider.js
@@ -2,31 +2,37 @@ export const render = () => {
   const hook = document.querySelector('#hook')
   const pictures = [
     {
+      id: 1,
       name: 'img1.JPG',
       url: '../assets/img1.JPG',
       alt: 'Cirsium Arvense',
     },
     {
+      id: 2,
       name: 'img2.JPG',
       url: '../assets/img2.JPG',
       alt: 'Cirsium Oleraceum',
     },
     {
+      id: 3,
       name: 'img3.JPG',
       url: '../assets/img3.JPG',
       alt: 'Eupatorium Cannabinum',
     },
     {
+      id: 4,
       name: 'img4.JPG',
       url: '../assets/img4.JPG',
       alt: 'Heracleum sphondylium',
     },
     {
+      id: 5,
       name: 'img5.JPG',
       url: '../assets/img5.JPG',
       alt: 'Calystegia sepium',
     },
     {
+      id: 6,
       name: 'img6.JPG',
       url: '../assets/img6.JPG',
       alt: 'Calystegia sepium',
@@ -34,6 +40,7 @@ export const render = () => {
   ]
   /* hook.innerHTML = '<h1>Hello<h1>' */
   slideUi(hook, pictures)
+  showSlide(pictures);
 }
 
 const slideUi = (hook, images) => {
@@ -42,11 +49,13 @@ const slideUi = (hook, images) => {
 
   images.map((image) => {
     const slide = document.createElement('div')
-    slide.classList.add('slide')
+    slide.classList.add('slides')
+    slide.setAttribute('data-number', `${image.id}`)
 
     const picture = document.createElement('img')
     picture.setAttribute('src', `${image.url}`)
     picture.setAttribute('alt', `${image.alt}`)
+    picture.setAttribute('data-number', `${image.id}`)
     picture.classList.add('pictures')
 
     slide.appendChild(picture)
@@ -54,4 +63,31 @@ const slideUi = (hook, images) => {
   })
 
   hook.appendChild(slideContainer)
-} 
+}
+
+const showSlide = (images) => {
+  const imgs = document.querySelectorAll('.slides') 
+
+  let i = 3
+  const show = images.find(image => image.id === i)
+  console.log(show)
+  const imgA = Array.from(imgs)
+  console.log(imgA)
+  console.log(imgA.find(img => img.dataset.number === String(i)))
+  const b = imgA.find(img => img.dataset.number === String(i))
+  b.classList.toggle('active')
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
